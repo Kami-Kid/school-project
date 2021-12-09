dd01a0879266a8402b13af13fac11fa6534fed3a
Kami-Kid <55991137+Kami-Kid@users.noreply.github.com>
2021-12-09 13:59:07 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -11,47 +11,46 @@ var n4 = 0;
 var n5 = 0;
 
 
-var pool = [" Kylie", " Harry N", " Jessica", " Eleanor", " Hugo", " Jennifer", " Natalia", " Aaminah", " Ahsan", " Helin", " Khabab", " Emily", " Clara", " Phoebe", " Matthew", " Amelia", " Lily", " Jasmine", " Sara", " Ari", " Hilde", " Nadav", " Storm", " Harry W", " Isaac", " Jakub", " Raphie"];
+var pool = [" Hasan"," Samuel"," Luca", " Kylie", " Harry N", " Jessica", " Eleanor", " Hugo", " Jennifer", " Natalia", " Aaminah", " Ahsan", " Helin", " Khabab", " Emily", " Clara", " Phoebe", " Matthew", " Amelia", " Lily", " Jasmine", " Sara", " Ari", " Hilde", " Nadav", " Storm", " Harry W", " Isaac", " Jakub", " Raphie"];
 
 function randomiser() {
     for (i = 0; i < pool.length; i++) {
         switch (Math.round((Math.random() * 4) + 1)) {
             case 1:
-                if (n1 != 6) {
+                if (n1 != 7) {
                     g1.push(pool[1])
                     pool.shift()
                     n1++
                 }
                 break
             case 2:
-                if (n2 != 5) {
+                if (n2 != 7) {
                     g2.push(pool[1])
                     pool.shift()
                     n2++
                 }
                 break
             case 3:
-                if (n3 != 6) {
+                if (n3 != 7) {
                     g3.push(pool[1])
                     pool.shift()
                     n3++
                 }
                 break
             case 4:
-                if (n4 != 6) {
+                if (n4 != 7) {
                     g4.push(pool[1])
                     pool.shift()
                     n4++
                 }
                 break
             case 5:
-                if (n5 != 6) {
+                if (n5 != 7) {
                     g5.push(pool[1])
                     pool.shift()
                     n5++
                 }
                 break
-                //console.log(Math.round((Math.random() * 4) + 1))
         }
 
     }
@@ -64,9 +63,6 @@ function begin() {
         while (pool.length != 0) {
             randomiser()
         }
-        g3.push(" Hasan")
-        g4.push(" Samuel")
-        g5.push(" Luca")
         document.getElementById("g1").innerHTML += g1
         document.getElementById("g2").innerHTML += g2
         document.getElementById("g3").innerHTML += g3
