b1f03cd188550399175f1c4f1b760e3a9379d2d7
Kami-Kid <55991137+Kami-Kid@users.noreply.github.com>
2021-12-16 00:05:19 +0000

    Add files via upload
    i added a reload and removed some bugs

--- main.js
+++ main.js
@@ -7,46 +7,46 @@ var g5 = [];
 var n1 = 0;
 var n2 = 0;
 var n3 = 0;
-var n4 = 1;
-var n5 = 1;
+var n4 = 0;
+var n5 = 0;
 
 
-var pool = ["hi"," Luca", " Harry N", " Samuel", " Kylie"," Hasan", " Jessica", " Eleanor", " Hugo", " Jennifer", " Natalia", " Aaminah", " Ahsan", " Helin", " Khabab", " Emily", " Clara", " Phoebe", " Matthew", " Amelia", " Lily", " Jasmine", " Sara", " Ari", " Hilde", " Nadav", " Storm", " Harry W", " Isaac", " Jakub", " Raphie"];
+var pool = [" Luca", " Harry N", " Samuel", " Kylie", " Hasan", " Jessica", " Eleanor", " Hugo", " Jennifer", " Natalia", " Aaminah", " Ahsan", " Helin", " Khabab", " Emily", " Clara", " Phoebe", " Matthew", " Amelia", " Lily", " Jasmine", " Sara", " Ari", " Hilde", " Nadav", " Storm", " Harry W", " Isaac", " Jakub", " Raphie"];
 
 function randomiser() {
-    for (i = 0; i < pool.length; i++) {
+    for (i = 0; i < 30; i++) {
         switch (Math.round((Math.random() * 4) + 1)) {
             case 1:
                 if (n1 != 6) {
-                    g1.push(pool[1])
+                    g1.push(pool[0])
                     pool.shift()
                     n1++
                 }
                 break
             case 2:
-                if (n2 != 5) {
-                    g2.push(pool[1])
+                if (n2 != 6) {
+                    g2.push(pool[0])
                     pool.shift()
                     n2++
                 }
                 break
             case 3:
-                if (n3 != 5) {
-                    g3.push(pool[1])
+                if (n3 != 6) {
+                    g3.push(pool[0])
                     pool.shift()
                     n3++
                 }
                 break
             case 4:
                 if (n4 != 6) {
-                    g4.push(pool[1])
+                    g4.push(pool[0])
                     pool.shift()
                     n4++
                 }
                 break
             case 5:
                 if (n5 != 6) {
-                    g5.push(pool[1])
+                    g5.push(pool[0])
                     pool.shift()
                     n5++
                 }
@@ -60,6 +60,9 @@ function randomiser() {
 predone = false
 
 function begin() {
+    if (predone == true) {
+        location.reload()
+    }
     if (predone == false) {
         while (pool.length != 0) {
             randomiser()
